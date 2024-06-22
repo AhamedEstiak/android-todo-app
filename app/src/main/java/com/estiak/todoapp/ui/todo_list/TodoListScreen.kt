e46e3e56ce6af5766462b0b5c3d6426eccab74c6
Estiak ahamed <aiubpolin@gmail.com>
2024-06-22 18:06:36 +0600

    snackbar dismiss added

--- app/src/main/java/com/estiak/todoapp/ui/todo_list/TodoListScreen.kt
+++ app/src/main/java/com/estiak/todoapp/ui/todo_list/TodoListScreen.kt
@@ -1,9 +1,13 @@
 package com.estiak.todoapp.ui.todo_list
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -11,6 +15,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
@@ -19,8 +24,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.estiak.todoapp.utils.UiEvent
 
@@ -39,9 +49,11 @@ fun TodoListScreen(
         viewModel.uiEvent.collect { event ->
             when (event) {
                 is UiEvent.ShowSnackBar -> {
+                    snackbarHostState.currentSnackbarData?.dismiss()
                     val result = snackbarHostState.showSnackbar(
                         message = event.message,
-                        actionLabel = event.action
+                        actionLabel = event.action,
+                        duration = SnackbarDuration.Short
                     )
 
                     if (result == SnackbarResult.ActionPerformed) {
@@ -68,7 +80,7 @@ fun TodoListScreen(
         }
     ) { paddingValues ->
         if (todos.value.isEmpty()) {
-            Text(text = "No todos found")
+            EmptyList()
         } else {
             LazyColumn(
                 Modifier
@@ -90,5 +102,22 @@ fun TodoListScreen(
             }
         }
     }
+}
 
+@Composable
+@Preview
+fun EmptyList() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "No todos found",
+            fontSize = 18.sp,
+            color = Color.DarkGray,
+            fontWeight = FontWeight.Medium
+        )
+    }
 }
